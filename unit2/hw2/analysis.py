67f200b339eb2c11ceafd30971b26813c459839c
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-12 18:16:56 -0500

    add working parameters for q3a

--- unit2/hw2/analysis.py
+++ unit2/hw2/analysis.py
@@ -25,9 +25,9 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.1
+    answerNoise = 0.0025
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
